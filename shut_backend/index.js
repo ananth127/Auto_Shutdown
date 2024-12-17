44872dbbd0f70834be6e051f7b9d6d318375fabb
ananth127 <ananths12704@gmail.com>
2024-12-17 14:08:13 +0530

    second commit

--- shut_backend/index.js
+++ shut_backend/index.js
@@ -86,7 +86,7 @@ app.post('/toggle-shutdown', async (req, res) => {
         user.shutdown = false;
         await user.save();
         console.log(`Shutdown reset to false for user ${uniqueId}`);
-      }, 20000); // 1 minute = 60000 ms
+      }, 11000); // 1 minute = 60000 ms
     }
   } catch (error) {
     console.error(error);
